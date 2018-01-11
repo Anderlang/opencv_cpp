d449f3961e67c53dd25a7e3cfe552df1aeae793e
Anderlang <itbjm@sina.com>
2018-01-11 21:30:23 +0800

    添加灰度示例

--- main.cpp
+++ main.cpp
@@ -2,13 +2,18 @@
 using namespace cv;
 using namespace std;
 int main(){
+  //读图片
   Mat img=imread("../mm.jpg");
   if(!img.data){
     cout<<"error";
     return -1;
   }
-  namedWindow("demo");
+  //窗体,可以不使用下面这句
+  // namedWindow("demo"); 
   imshow("demo",img);
+  Mat dst;
+   cvtColor(img,dst,CV_BGR2GRAY);
+    imshow("灰度",dst);
   waitKey();
   return 0;
 }
